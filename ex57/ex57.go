b13a3ee4d7f5037e39cc741a44959ad861e89ef6
Fred Yankowski <fcy@imsa.edu>
2013-09-24 10:25:41 -0500

    Reformat.

--- ex57/ex57.go
+++ ex57/ex57.go
@@ -1,29 +1,32 @@
 package main
 
 import (
-    "net/http"
-    "fmt"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 type String string
+
 func (s String) ServeHTTP(w http.ResponseWriter,
-                           r *http.Request) {
+	r *http.Request) {
 	fmt.Fprint(w, s)
 }
 
 type Struct struct {
-    Greeting string
-    Punct    string
-    Who      string
+	Greeting string
+	Punct    string
+	Who      string
 }
+
 func (s Struct) ServeHTTP(w http.ResponseWriter,
-			r *http.Request) {
+	r *http.Request) {
 	fmt.Fprintf(w, "%v%v %v", s.Greeting, s.Punct, s.Who)
 }
 
 func main() {
-    http.Handle("/string", String("I'm a frayed knot."))
-    http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
-    // your http.Handle calls here
-    http.ListenAndServe("localhost:4000", nil)
+	http.Handle("/string", String("I'm a frayed knot."))
+	http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+	// your http.Handle calls here
+	log.Fatal(http.ListenAndServe("app.devnet.imsa.edu:4000", nil))
 }
